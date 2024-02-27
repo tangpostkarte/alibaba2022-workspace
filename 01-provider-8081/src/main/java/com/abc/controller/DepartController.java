6f6f1d830c7433ead7b10df014e75155acf5388c
Your Name <you@example.com>
2024-02-28 06:49:21 +0800

    add datasource

--- 01-provider-8081/src/main/java/com/abc/controller/DepartController.java
+++ 01-provider-8081/src/main/java/com/abc/controller/DepartController.java
@@ -3,20 +3,40 @@ package com.abc.controller;
 import com.abc.bean.Depart;
 import com.abc.service.DepartService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-@RequestMapping("/depart")
+import java.util.List;
+
+@RequestMapping("/provider/depart")
 @RestController
 public class DepartController {
 
     @Autowired
     private DepartService service;
 
-    @PostMapping("/")
+    @PostMapping("/save")
     public boolean saveHandle(@RequestBody Depart depart) {
         return service.saveDepart(depart);
     }
+
+    @DeleteMapping("/del/{id}")
+    public boolean deleteHandle(@PathVariable("id") int id) {
+        return service.removeDepartById(id);
+    }
+
+    @PutMapping("/update")
+    public boolean updateHandle(@RequestBody Depart depart) {
+        return service.modifyDepart(depart);
+    }
+
+    @GetMapping("/get/{id}")
+    public Depart getHandle(@PathVariable("id") int id) {
+        return service.getDepartById(id);
+    }
+
+    @GetMapping("/list")
+    public List<Depart> listHandle() {
+        return service.listAllDeparts();
+    }
+
 }
